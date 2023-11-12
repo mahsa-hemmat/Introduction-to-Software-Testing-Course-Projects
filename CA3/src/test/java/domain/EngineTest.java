7ff3b082845d76addf143d76f5a38b2b1f6538d2
Mahsa hemmatpanah <hemmatp.m@gmail.com>
2023-11-12 20:25:29 +0330

    EngineTest.jav Updated

--- CA3/src/test/java/domain/EngineTest.java
+++ CA3/src/test/java/domain/EngineTest.java
@@ -15,6 +15,7 @@ class EngineTest {
         engine = new Engine();
     }
 
+    /*
     @Test
     void testAverageOrderQuantityByCustomerWhenNoOrders() {
         int average = engine.getAverageOrderQuantityByCustomer(1);
@@ -140,7 +141,7 @@ class EngineTest {
         int fraudulentQuantity = engine.getCustomerFraudulentQuantity(order2);
 
         assertEquals(5, fraudulentQuantity);
-    }
+    }*/
 
 
     @Test
@@ -157,7 +158,7 @@ class EngineTest {
     }
 
     @Test
-    void testAddOrderAndGetFraudulentQuantityWithFraudulentQuantity() {
+    void testAddOrderAndGetFraudulentQuantityCase1() {
         Order order = new Order();
         order.setCustomer(1);
         order.setQuantity(15);
@@ -169,15 +170,35 @@ class EngineTest {
     }
 
     @Test
-    void testAddOrderAndGetFraudulentQuantityWithPattern() {
+    void testAddOrderAndGetFraudulentQuantityCase2() {
         Order order1 = new Order();
         order1.setId(1);
+        order1.setCustomer(2);
+        order1.setPrice(100);
+        order1.setQuantity(10);
+        engine.orderHistory.add(order1);
+
+        Order order = new Order();
+        order.setId(2);
+        order.setCustomer(1);
+        order.setPrice(100);
+        order.setQuantity(9);
+
+        assertThrows(java.lang.ArithmeticException.class, ()->engine.addOrderAndGetFraudulentQuantity(order), "/ by zero");
+    }
+
+    @Test
+    void testAddOrderAndGetFraudulentQuantityCase3() {
+        Order order1 = new Order();
+        order1.setId(1);
+        order1.setCustomer(1);
         order1.setPrice(100);
         order1.setQuantity(10);
         engine.orderHistory.add(order1);
 
         Order order2 = new Order();
         order2.setId(2);
+        order2.setCustomer(1);
         order2.setPrice(100);
         order2.setQuantity(5);
 
@@ -185,5 +206,121 @@ class EngineTest {
 
         assertEquals(0, fraudulentQuantity);
         assertEquals(2, engine.orderHistory.size());
+    }
+    @Test
+    void testAddOrderAndGetFraudulentQuantityCase4() {
+        Order order1 = new Order();
+        order1.setId(1);
+        order1.setCustomer(1);
+        order1.setPrice(10);
+        order1.setQuantity(5);
+        Order order2 = new Order();
+        order2.setId(2);
+        order1.setCustomer(1);
+        order2.setPrice(50);
+        order2.setQuantity(5);
+        Order order3 = new Order();
+        order3.setId(3);
+        order1.setCustomer(1);
+        order3.setPrice(100);
+        order3.setQuantity(3);
+        engine.orderHistory.add(order1);
+        engine.orderHistory.add(order2);
+
+        int fraudulentQuantity = engine.addOrderAndGetFraudulentQuantity(order3);
+
+        assertEquals(0, fraudulentQuantity);
+        assertEquals(3, engine.orderHistory.size());
+    }
+
+    @Test
+    void testAddOrderAndGetFraudulentQuantityCase5() {
+        Order order1 = new Order();
+        order1.setId(1);
+        order1.setCustomer(1);
+        order1.setPrice(10);
+        order1.setQuantity(5);
+        Order order2 = new Order();
+        order2.setId(2);
+        order1.setCustomer(1);
+        order2.setPrice(100);
+        order2.setQuantity(5);
+        Order order3 = new Order();
+        order3.setId(3);
+        order1.setCustomer(1);
+        order3.setPrice(100);
+        order3.setQuantity(3);
+        engine.orderHistory.add(order1);
+        engine.orderHistory.add(order2);
+
+        int fraudulentQuantity = engine.addOrderAndGetFraudulentQuantity(order3);
+
+        assertEquals(0, fraudulentQuantity);
+        assertEquals(3, engine.orderHistory.size());
+    }
+
+    @Test
+    void testAddOrderAndGetFraudulentQuantityCase6() {
+        Order order1 = new Order();
+        order1.setId(1);
+        order1.setCustomer(1);
+        order1.setPrice(10);
+        order1.setQuantity(5);
+        Order order2 = new Order();
+        order2.setId(2);
+        order1.setCustomer(1);
+        order2.setPrice(100);
+        order2.setQuantity(10);
+        Order order3 = new Order();
+        order3.setId(3);
+        order1.setCustomer(1);
+        order3.setPrice(100);
+        order3.setQuantity(10);
+        Order order = new Order();
+        order.setId(4);
+        order.setCustomer(1);
+        order.setPrice(100);
+        order.setQuantity(3);
+        engine.orderHistory.add(order1);
+        engine.orderHistory.add(order2);
+        engine.orderHistory.add(order3);
+
+        int fraudulentQuantity = engine.addOrderAndGetFraudulentQuantity(order);
+
+        assertEquals(0, fraudulentQuantity);
+        assertEquals(4, engine.orderHistory.size());
+    }
+
+    @Test
+    void testAddOrderAndGetFraudulentQuantityCase7() {
+        Order order1 = new Order();
+        order1.setId(1);
+        order1.setCustomer(1);
+        order1.setPrice(10);
+        order1.setQuantity(5);
+        Order order2 = new Order();
+        order2.setId(2);
+        order1.setCustomer(1);
+        order2.setPrice(100);
+        order2.setQuantity(10);
+        Order order3 = new Order();
+        order3.setId(3);
+        order1.setCustomer(1);
+        order3.setPrice(100);
+        order3.setQuantity(15);
+        order3.setQuantity(15);
+        Order order = new Order();
+        order.setId(4);
+        order.setCustomer(1);
+        order.setPrice(100);
+        order.setQuantity(3);
+        engine.orderHistory.add(order1);
+        engine.orderHistory.add(order2);
+        engine.orderHistory.add(order3);
+
+        int fraudulentQuantity = engine.addOrderAndGetFraudulentQuantity(order);
+
+        assertEquals(5, fraudulentQuantity);
+        assertEquals(4, engine.orderHistory.size());
     }
 }
